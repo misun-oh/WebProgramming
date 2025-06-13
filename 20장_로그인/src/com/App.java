b62fe02231e112a91ef9bcc6a18f90e4a260095d
momo <momo.class5@gmail.com>
2025-06-13 09:48:30 +0900

    로그인

--- 20장_로그인/src/com/App.java
+++ 20장_로그인/src/com/App.java
@@ -4,6 +4,9 @@ import com.lib.AllInOne;
 
 public class App {
 	public static void main(String[] args) {
+		System.out.println("==========");
+		System.out.println("   성남 아트리움 도서관에 오신걸 환영 합니다.");
+		System.out.println("==========");
 		AllInOne allInOne = new AllInOne();
 		allInOne.login();
 	}
--- 20장_로그인/src/com/lib/AllInOne.java
+++ 20장_로그인/src/com/lib/AllInOne.java
@@ -11,6 +11,7 @@ public class AllInOne {
 	 * 사용자로 부터 ID, PW를 입력 받아서 사용자가 있는지 확인하는 작업
 	 */
 	public void login() {
+		System.out.println("로그인");
 		System.out.print("id : ");
 		String id = s.next();
 		System.out.print("pw : ");
